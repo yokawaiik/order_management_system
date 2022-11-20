c340944ef26b3086f2ca950c435e782493cd18d8
Danil Shubin <yokawaiik@gmail.com>
2022-11-21 00:31:01 +0700

    Set up the environment.

---
+++ smart_contracts/hardhat.config.ts
@@ -0,0 +1,20 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import"@nomiclabs/hardhat-ethers";
+import"@nomicfoundation/hardhat-chai-matchers";
+
+
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+};
+
+export default config;
